d329a852fc5bffc8e9d5f97d54e9351a0f7eb6a5
sandina <alejandro.sandin@roche.com>
2022-01-11 13:01:45 +0100

    Initial commit

---
+++ src/test/java/com/sandin/learning/springbootstarting/SpringbootStartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.springbootstarting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
